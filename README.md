f28d5545db184d7fe347d853f5980a0d18e28bce
katherine-walton <katherinefaithwalton@gmail.com>
2020-03-24 22:32:45 -0700

    edits made for submission

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Web Proxy
+A simple web proxy that only implements a GET request
+#Instruction
+##How to run
+Run the following code in terminal while inside directory where python file is located. Be sure to replace <port number> with the port number you want the proxy to run on.
+```
+python3 proxy.py <port number>
+```
+##How to use
+Type a website address into a of your choice browser, following the example below:
+```
+localhost:<port number>/<website>
+```
+As an example
+```
+http://localhost:8888/www.google.com
+http://localhost:8080/www.stackoverflow.com
+```
--- proxy.py
+++ proxy.py
@@ -3,59 +3,53 @@ import sys
 import argparse
 
 def proxy(port_number):
-	sock = socket(AF_INET, SOCK_STREAM)
-	sock.bind(('', port_number))
-	sock.listen(1)
+	proxy_socket = socket(AF_INET, SOCK_STREAM)
+	proxy_socket.bind(('', port_number))
+	proxy_socket.listen(1)
 	while True:
 		# open a socket connection on startup and listen for incoming requests
-		connection, client_address = sock.accept()
+		connection, addr = proxy_socket.accept()
 		print("Connected")
-		try:
-			while True:
-				# on getting a request from the browser, proxy should parse the HTTP request to determine the destination server
-				data = connection.recv(1024)
-				if data:
-					print("http request from browser\n", data)
-					first_line = data.decode().split('\n')[0].split()
-					# parse the data for the destination
-					if first_line[0] == 'GET':
-						print("valid GET request received")
+		# on getting a request from the browser, proxy should parse the HTTP request to determine the destination server
+		data = connection.recv(1024)
+		if data:
+			print("http request from browser\n", data)
+			first_line = data.decode().split('\n')[0].split()
+			# parse the data for the destination
+			if first_line[0] == 'GET':
+				print("valid GET request received")
 
-						# make connection to destination server
-						split_address = first_line[1][1:].split('/', 1)
-						domain = split_address[0]
-						if len(split_address) > 1:
-							url = split_address[1]
-						else:
-							url='/'
-						remote_host = (domain, 80)
-						print("About to connect to ", domain)
-						s = socket(AF_INET, SOCK_STREAM)
+				# make connection to destination server
+				split_address = first_line[1][1:].split('/', 1)
+				domain = split_address[0]
+				if len(split_address) > 1:
+					url = split_address[1]
+				else:
+					url='/'
+				
+				print("About to connect to ", domain)
+				s = socket(AF_INET, SOCK_STREAM)
+				remote_host = (domain, 80)
+				s.connect(remote_host)
 
-						s.connect(("www.stackoverflow.com", 80))
-						# s.connect(remote_host)
+				# send request for page
+				request = "GET /{} HTTP/1.1\r\nHost: {}\r\n\r\n"
+				s.sendall(request.format(url, domain).encode())
 
-						# send request for page
-						request = "GET /{} HTTP/1.1\r\nHost: {}\r\n\r\n"
-						s.sendall(request.format(url, domain).encode())
-
-						while True:
-							# process reply
-							receive = s.recv(1024)
-							print(repr(receive))
-							if receive:
-								print("Sending page to browser")
-								# send reply to browser
-								connection.send(receive)
-							else:
-								break
+				while True:
+					# process reply
+					receive = s.recv(1024)
+					print(repr(receive))
+					if receive:
+						print("Sending page to browser")
+						# send reply to browser
+						connection.send(receive)
 					else:
-						print('Error: not implemented')
-						# TODO should I be breaking?
 						break
+			else:
+				print('Error: not implemented')
+				break
 
-		finally:
-			connection.close()
 
 def main():
 	# get port number from command line
