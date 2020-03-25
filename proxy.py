820f8f658501fc740644bdc94b5536a95ba36b3d
katherine-walton <katherinefaithwalton@gmail.com>
2020-03-24 21:29:08 -0700

    Receive http request from browser

--- proxy.py
+++ proxy.py
@@ -1,37 +1,62 @@
-import socket
 from socket import *
-import argparse 
+import sys
+import argparse
+
+def proxy(port_number):
+	sock = socket(AF_INET, SOCK_STREAM)
+	sock.bind(('', port_number))
+	sock.listen(1)
+	while True:
+		# open a socket connection on startup and listen for incoming requests
+		connection, client_address = sock.accept()
+		print("Connected")
+		try:
+			while True:
+				# on getting a request from the browser, proxy should parse the HTTP request to determine the destination server
+				data = connection.recv(1024)
+				if data:
+					print("http request from browser\n", data)
+					first_line = data.decode().split('\n')[0].split()
+					# parse the data for the destination
+					if first_line[0] == 'GET':
+						print("valid GET request received")
+
+						# make connection to destination server
+						domain = first_line[1][1:]
+						remote_host = (domain, 80)
+						print("About to connect to ", domain)
+						s = socket(AF_INET, SOCK_STREAM)
+						s.connect(remote_host)
+
+						# send request for page
+						# TODO don't hard code this
+						s.sendall("GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n".encode())
+
+						while True:
+							# process reply
+							receive = s.recv(1024)
+							print(repr(receive))
+							if receive:
+								print("Sending page to browser")
+								# send reply to browser
+								connection.send(receive)
+							else:
+								break
+					else:
+						print('Error: not implemented')
+						# TODO should I be breaking?
+						break
+
+		finally:
+			connection.close()
 
 def main():
-	# serverName = 'hostname'
-	# serverPort = 12000
-	# serverSocket = socket(AF_INET, SOCK_DGRAM)
-	# serverSocket.bind(('', serverPort))
-	# # message = raw_input('Input lowercase sentence:')
-	# # clientSocket.sendto(message.encode(), (serverName, serverPort))
-	# # print(serverPost)
-
+	# get port number from command line
 	parser = argparse.ArgumentParser()
 	parser.add_argument('port_number', nargs='?', type=int, default=8080)
 	args=parser.parse_args()
 
-	# print('Port Number')
-	# print(args.port_number)
-
-	# get port number from command line
-	port_number = args.port_number
-
-	# open a socket connection on startup and listen for incoming requests
-
-	# on getting a request from the browser, proxy should parse the HTTP request to determine the destination server
-
-	# open a connection to destination server
-
-	# send request
-
-	# process reply and send to browser
-
-
+	proxy(args.port_number)
 
 if __name__ == "__main__":
     main()
